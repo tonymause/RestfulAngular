8a953c1eebff846471159d8e56ab913faaf7b41c
weibin <tonymause@163.com>
2016-03-06 23:55:03 +0800

    added gulp scripts and Angular structures

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
 
+// clean files first
+gulp.task('clean', function(){
+  gulp.src('./www/minify')
+    .pipe(clean());
+});
 
 // copy files
 gulp.task('copy', function(){
@@ -11,4 +20,25 @@ gulp.task('copy', function(){
     .pipe(gulp.dest('www/css/'));
 });
 
-gulp.task('default', ['copy']);
+// minify task
+gulp.task('minify-html', function () {
+    gulp.src('./www/*.html') // path to your files
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('./www/minify/html'));
+});
+
+gulp.task('minify-css', function(){
+    gulp.src('./www/css/*.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./www/minify/css'))
+});
+
+gulp.task('minify-js', function(){
+    gulp.src('./www/js/*/**')
+    .pipe(uglify())
+    .pipe(gulp.dest('./www/minify/'))
+})
+
+gulp.task('minify', ['minify-html', 'minify-css']);
+
+gulp.task('default', ['clean', 'copy', 'minify']);
